74eec7a0e216056d395d43b9d2b198913a69eb8f
Pratim Vengurlekar <Pratim Vengurlekar@LAPTOP-IGJAR46E>
2025-03-11 00:49:38 +0530

    6th Mar- I am Pushing the test code to github

--- src/Ex_01_JavaBasic/Lab002_test.java
+++ src/Ex_01_JavaBasic/Lab002_test.java
@@ -1,8 +1,7 @@
 package Ex_01_JavaBasic;
 
-public class Lab001_HelloWorld {
+public class Lab002_test {
     public static void main(String[] args) {
-        System.out.println("Hello World1");
-
+        System.out.println("Test");
     }
 }
