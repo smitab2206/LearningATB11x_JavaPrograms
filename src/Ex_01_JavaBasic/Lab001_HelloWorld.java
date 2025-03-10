3648f1426d1e5b05a6ae52dc682ad187c6a68dbe
Pratim Vengurlekar <Pratim Vengurlekar@LAPTOP-IGJAR46E>
2025-03-10 21:38:59 +0530

    6th Mar- I am Pushing the test code

--- src/Ex_01_JavaBasic/Lab001_HelloWorld.java
+++ src/Ex_01_JavaBasic/Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package Ex_01_JavaBasic;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
     }
 }
